d223599a01fc5012c97299a560fd3b558ee40c65
Lieby Cardoso <liebycardoso@gmail.com>
2019-11-21 18:17:57 -0500

    description user

--- publisher.py
+++ publisher.py
@@ -14,7 +14,6 @@ from google.cloud import pubsub_v1
 
 # Get twitter credentials from file.
 
-
 consumer_key = os.environ['CONSUMERKEY']
 consumer_secret = os.environ['CONSUMERSECRET']
 access_token = os.environ['ACCESSTOKEN']
@@ -24,8 +23,9 @@ PROJECT_ID = os.environ['PROJECT_ID']
 TWSTREAMMODE = os.environ['TWSTREAMMODE']
 
 NUM_RETRIES = 3
-NUMBER_OF_TWEETS = 4000
+NUMBER_OF_TWEETS = 10000
 BATCH_SIZE = 50
+FEATURES_LEN = 16
 
 def publish(client, topic_path, data_lines):
     """Publish to the given pubsub topic."""
@@ -37,8 +37,18 @@ def publish(client, topic_path, data_lines):
     data = base64.urlsafe_b64encode(bytearray(str_body, 'utf8'))
     client.publish(topic_path, data=data)
 
-def filter_data(data, type):
+def filter_data(data):
+    txt = ''
     data = json.loads(data)
+    
+
+    if ("extended_tweet"  in data):
+        txt = data.get("extended_tweet", {}).get('full_text', "null")
+    elif ("full_text" in data): 
+        txt = data.get('full_text',"")
+    else:
+        txt = data.get('text',"")
+
     try:  
         
         # filter only meaningful features 
@@ -48,7 +58,7 @@ def filter_data(data, type):
             data = (                
                         str(datetime.strptime(data["created_at"], '%a %b %d %H:%M:%S %z %Y')),
                         int(data.get("id",0)),
-                        data.get(type),
+                        txt,
                         int(data.get("quote_count", 0)),
                         int(data.get("reply_count", 0)),
                         int(data.get("retweet_count", 0)),
@@ -61,7 +71,7 @@ def filter_data(data, type):
                         int(data.get("user", {}).get("listed_count",0)),
                         int(data.get("user", {}).get("favourites_count",0)),
                         int(data.get("user", {}).get("statuses_count",0)),                            
-                        int(data.get("user", {}).get("description",0))
+                        data.get("user", {}).get("description","null")
                     )          
     except Exception as e:
         print(e)
@@ -77,11 +87,11 @@ def process_timeline(username, n_weets):
                              tweet_mode="extended",
                              languages=["en"]).items(n_weets):
         try:
-            data = filter_data(json.dumps(item._json),"full_text")
-            if len(data) <= 15:
+            data = filter_data(json.dumps(item._json))
+            if len(data) <= FEATURES_LEN:
                 tml.append(data)
             
-            if len(tml) >= BATCH_SIZE:
+            if len(tml) <= BATCH_SIZE:
                 publish(publisher, PUBSUB_TOPIC, tml)
                 tml = []
 
@@ -116,8 +126,8 @@ class StdOutListener(StreamListener):
         named methods, but these methods are only stubs.
         """
         
-        data = filter_data(data, "text")
-        if len(data) <= 15:
+        data = filter_data(data)
+        if len(data) <= FEATURES_LEN:
             self.tweets.append(data)
 
                 
@@ -159,11 +169,12 @@ if __name__ == '__main__':
     #follow=['14260960', '253340075', '256360738'])
     #['JustinTrudeau', 'AndrewScheer', 'theJagmeetSingh']
     
+  
     if TWSTREAMMODE == "timeline":
         process_timeline("JustinTrudeau", NUMBER_OF_TWEETS)
         process_timeline("AndrewScheer", NUMBER_OF_TWEETS)
         process_timeline("theJagmeetSingh", NUMBER_OF_TWEETS)
-
+   
     stream.filter(languages=["en"],
             track=['#cdnpoli', '#elxn43','#CanadaElection2019', 
             '#canpoli', '#CanadianElection', '#JustinTrudeau',
